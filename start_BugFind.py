14df9b574ae4f16a65834269b7e2490829dd3929
Sabine Zach <zachse@pslap08.informatik.uni-stuttgart.de>
2020-07-12 20:25:20 +0200

    Skript for starting BugFind.py

---
+++ start_BugFind.py
@@ -0,0 +1,190 @@
+'''
+Created on July 07, 2020
+
+@author: Sabine Zach
+
+Last Changed on July 10, 2020
+
+@by: Sabine Zach
+'''
+# Skript for Testing Bug_Find
+# Tests for all bug patterns
+# Data: data/js/programs_50
+
+import sys
+import json
+import math
+import math
+import time
+from os import getcwd
+from os import remove
+from os.path import exists
+from shutil import copyfile
+
+from subprocess import call, check_output
+
+##--------------------------------------------------------
+def get_all_js_files(txt_file):
+    ##--------get input data file names out of text file----
+    all_js_files = ''
+    p50 = open(txt_file, 'r+')
+    for js_name in p50:
+        all_js_files = all_js_files + ' ' + js_name.replace('\n','')
+    return all_js_files
+##--get all js file names---------------------------------
+
+##--------------------------------------------------------
+def what_to_bugspec(what):
+    bugspec = "calls"             #not entirely accurate
+    if what == "SwappedArgs":
+        bugspec = "calls"
+    elif what == "BinOperator":
+        bugspec = "binOps"
+    elif what == "IncorrectBinaryOperand":
+        bugspec = "binOps"
+    elif what == "SwappedBinOperands":
+        bugspec = "binOps"
+    #--todo
+    #elif what == "MissingArg":
+    #    bugspec = "????"
+    #elif what == "IncorrectAssignment":
+    #    bugspec = "????"
+    else:
+        pass
+    return bugspec
+##--change 'what' to 'bugspec'----------------------------
+
+##--------------------------------------------------------
+def extractFromJS(bugspec, all_js_data_files):
+    ##----------------------------------------------------
+    ##  produces calls_xx*.json
+    ##       or  binOps_xx*.json files
+    ##  see javascript/extractFromJS.js
+    ##----------------------------------------------------
+
+    ##----create outfile name----
+    time_stamp = math.floor(time.time() * 1000)
+    outfile_json = bugspec + '_' + str(time_stamp) + ".json"
+    outfile_str = '--outfile ' + outfile_json
+
+    ##extract command
+    data_extract_str = 'node ./javascript/extractFromJS.js ' + bugspec + ' --files '
+
+    ##execute extract command
+    print( '%s %s %s' %(data_extract_str, all_js_data_files, outfile_str))
+    returned = call( '%s %s %s' %(data_extract_str, all_js_data_files, outfile_str), shell=True)
+    if (returned != 0):
+        print('extractFromJS returned: %d' %(returned))
+
+    print('extractFromJS has returned with %d' %(returned))
+    print('Created file **%s**' % (outfile_json))
+
+    ##--todo--in case > 1 outfile:
+    ##return a string of all outfiles, separated by ' '
+    data_json = outfile_json
+    return data_json
+##------extractFromJS ------------------------------------
+
+##--------------------------------------------------------
+if __name__ == '__main__':
+    # arguments for start_BugFind: what
+    #  what is one of: SwappedArgs
+    #                  BinOperator
+    #                  SwappedBinOperands
+    #                  IncorrectBinaryOperand
+    #                  IncorrectAssignment
+    #                  MissingArg
+    #                  IncorrectAssignment
+    #
+
+    print("**start BugFind.py for all bug patterns**\n\n ")
+
+    ## use all bug patterns
+    bugpatterns = ("SwappedArgs", "BinOperator", "SwappedBinOperands", "IncorrectBinaryOperand")
+
+    for what in bugpatterns:
+        ##------------change 'what' to 'bugspec'----
+        ##  e.g.             'SwappedArgs' to 'calls'
+        bugspec = what_to_bugspec(what)
+        print("**" + what + "**\n")
+
+        ############################################
+        ## call extractFromJS for data/js/programs50
+        ############################################
+
+        ##-----get input data file names and extract code pieces-----
+        all_js_files = get_all_js_files('data/js/programs_50_all.txt')
+        str_of_json_files = extractFromJS(bugspec, all_js_files)
+
+        ##the command produces calls_*.json files, or binOps*.json files, etc.
+        ##depending on bugspec
+
+        #####################
+        ## end  extractFromJS
+        #####################
+
+        ## create arguments for BugFind:
+        ##     what --load <model file> <name to vector file> <type to vector file> <AST node type to vector file> --newData <list of data files in json format>
+        ## e.g.
+        ## SwappedArgs" "--load" "./SwappedArgs/bug_detection_model", "./token_to_vector.json", "./type_to_vector.json", "./node_type_to_vector.json", "--newData",  "./calls_1.json ./calls_2.json ./calls_3.json"
+
+        #####################
+        ## call BugFind
+        #####################
+        what
+        load = " --load"
+        model_file = " ./" + what + "/bug_detection_model"
+        token_to_vector_file = " ./token_to_vector.json"
+        type_to_vector_file = " ./type_to_vector.json"
+        node_type_to_vector_file = " ./node_type_to_vector.json"
+        new = " --newData"
+        json_files = ' ' + str_of_json_files
+
+        ##construct command for calling
+        call_bugfind_str = 'python3 ./python/BugFind.py ' + what + load + model_file + token_to_vector_file + type_to_vector_file + node_type_to_vector_file + new + json_files
+        print("calling BugFind:\n%s" %(call_bugfind_str))
+
+        ##execute command
+        ##--todo--  outfile_str
+        ##--returned = call( '%s %s' %(newdata_extract_str, outfile_str), shell=True)
+
+        returned = call( '%s' %(call_bugfind_str), shell=True)
+        ##check the unix exit code
+        if (returned != 0):
+            print('can not call BugFind.py, returned: %d' %(returned))
+            exit(1)
+        print('BugFind.py called successfully: %d' %(returned))
+
+        #####################
+        ## end  BugFind
+        #####################
+    
+        ## delete generated files #list_of_json_files
+        ##    ===>   delete after running BugFind.py!!!!!!!
+        print("delete ...................................\n")
+        list_of_json_files = str_of_json_files.split()
+        for json_file in list_of_json_files:
+            print("json_file ... : %s" %(json_file))
+            json_path = getcwd() + '/' + json_file
+            if exists(json_path):
+                remove(json_path)
+        ids_json_file = "fileIDs.json"
+        ids_json_path = getcwd() + '/' + ids_json_file
+        if exists(ids_json_path):
+            remove (ids_json_path)
+
+        ## save prediction file produced by BugFind
+        ## what - SwappedArgs, BinOperator, SwappedBinOperands, etc.
+        prediction_name = "predictions"
+        prediction_file = prediction_name + ".txt"
+        prediction_path = getcwd() + '/' + prediction_file
+        if exists(prediction_path):
+            print(prediction_path + " exists.\n")
+            ##----create outfile name----
+            time_stamp = math.floor(time.time() * 1000)
+            predict_t = prediction_name + '_' + str(time_stamp) + ".txt"
+            predict_t_path = getcwd() + '/' + predict_t
+            ## file copy !!!!
+            copyfile(prediction_path, predict_t_path)
+            remove (prediction_path)
+    ##--end------------------for what in bugPatterns##
